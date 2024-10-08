d50bcce71eaae17feff4bd87ad489378107d27aa
EriozDev <cilochemylan83@gmail.com>
2024-10-08 13:06:40 +0200

    modify(class.Client)

--- src/client/clientClass.lua
+++ src/client/clientClass.lua
@@ -6,6 +6,7 @@ function Client.Init(clientId)
     local self = {};
 
     self.client = clientId;
+    self.clientName = GetPlayerName(self.client);
     Idx = Idx + 1
     self.uniqueid = Idx;
     self.__ = {};
@@ -26,6 +27,14 @@ function Client:NetworkRegisterEvent(_e, _f)
     end)
 end
 
+function Client:getUserName()
+    return (self.clientName);
+end
+
+function Client:setProperties(pr)
+    self.__ = pr
+end
+
 function Client:getProperties()
     return self.__ or {};
 end
@@ -38,20 +47,16 @@ function Client:getState(k)
     return self.__[k] ~= nil
 end
 
-function Client:setProperties(pr)
-    self.__ = pr
-end
-
 function Client:getClientId()
     return (self.client);
 end
 
-function Client:getClientUniqueId()
-    return (self.uniqueid);
-end
-
 function Client:setUniqueId(uid)
     self.uniqueid = uid;
+end
+
+function Client:getClientUniqueId()
+    return (self.uniqueid);
 end
 
 function Client:destroy()
