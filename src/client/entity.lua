ef2bc7cffc2933e35ce5f0420a07d5e06f3c5ab1
EriozDev <cilochemylan83@gmail.com>
2024-10-16 21:07:27 +0200

    tweaks(entity)

--- src/client/entity.lua
+++ src/client/entity.lua
@@ -17,19 +17,3 @@ end
 function onGuard.Entity.GetState(k)
     return onGuard.Entity[k] ~= nil
 end
-
-onGuard.OnNet('onGuard:state:init', function(k, v)
-    if onGuard.Entity[k] == nil then
-        return
-    end
-
-    onGuard.Entity[k] = v
-end)
-
-onGuard.OnNet('onGuard:state:set', function(key, value)
-    if onGuard.Entity[key] == nil then
-        return
-    end
-
-    onGuard.Entity[key] = value
-end)
--- src/server/entity.lua
+++ src/server/entity.lua
@@ -17,20 +17,6 @@ onGuard.OnNet('onGuard:state:set', function(key, value)
     onGuard.Entity[key] = value
 end)
 
-function onGuard.Entity.State(k, v)
-    onGuard.Entity[k] = v
-    onGuard.TriggerClient('onGuard:state:init', k, v)
-end
-
-function onGuard.Entity.SetState(k, v)
-    if onGuard.Entity[k] == nil then
-        LOG.Error('This State is not valid then is not possible to set!')
-    else
-        onGuard.Entity[k] = v
-        onGuard.TriggerClient('onGuard:state:set', k, v)
-    end
-end
-
 function onGuard.Entity.GetState(k)
     return onGuard.Entity[k] ~= nil
 end
